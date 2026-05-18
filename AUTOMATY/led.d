4b90522bf8fd95207a336b8ff46b4ced41238a2a
Jakub Flisuniu <jakflis@student.agh.edu.pl>
2026-05-18 20:33:00 +0200

    Usuniecie zbednych plikow za pomoca clean target

--- AUTOMATY/led.d
+++
@@ -1,3 +0,0 @@
-.\led.o: led.c
-.\led.o: C:\Users\kubaf\AppData\Local\Keil_v5\ARM\Inc\Philips\LPC21xx.H
-.\led.o: led.h
--- PRZERWANIA/timer.d
+++
@@ -1,3 +0,0 @@
-.\timer.o: timer.c
-.\timer.o: C:\Users\kubaf\AppData\Local\Keil_v5\ARM\Inc\Philips\LPC21xx.H
-.\timer.o: timer.h
