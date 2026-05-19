ad82f4d01b0b810d46a478002c5481d36376d984
Jakub Flisuniu <jakflis@student.agh.edu.pl>
2026-05-19 16:26:22 +0200

    gotowe servo

---
+++ SERVO/main.c
@@ -0,0 +1,35 @@
+#include "servo.h"
+#include "led.h"
+#include "timer.h"
+#include "keyboard.h"
+
+int main(){
+	
+	DetectorInit();
+	KeyboardInit();
+	ServoInit(50);
+
+	while(1){
+		
+		switch(eKeyboardRead()){
+			case BUTTON_0:
+				ServoCallib();
+				break;
+			
+			case BUTTON_1:
+				ServoGoTo(12);
+				break;
+			
+			case BUTTON_2:
+				ServoGoTo(24);
+				break;
+			
+			case BUTTON_3:
+				ServoGoTo(36);
+				break;
+			
+			default:
+				break;
+		}
+	}
+}
